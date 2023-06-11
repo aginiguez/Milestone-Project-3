8b7fdabdcad392957f23437122203f1ab894e25f
Anthony Iniguez <aginiguez33@gmail.com>
2023-06-11 16:54:12 -0700

    -/+ to sign up

--- src/components/pages/CreateUser.jsx
+++ src/components/pages/CreateUser.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
 
-
 function CreateUser() {
     const [email, setUserEmail] = useState('');
     const [username, setUsername] = useState('');
